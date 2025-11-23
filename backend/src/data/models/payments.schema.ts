a324e2246d93ede5391ee20a5a2daeb59ce991eb
joo <youssifabozaid5@gmail.com>
2025-11-23 23:17:07 +0200

    Payment Schema

---
+++ backend/src/data/models/payments.schema.ts
@@ -0,0 +1,61 @@
+import {Schema , model,Document,Types} from "mongoose";
+
+export interface IPayment extends Document {
+    order: Types.ObjectId;
+    reservation?: Types.ObjectId | null ;/* Optional, for deposits------------------->*/
+    amount: number;
+    paymentMethod: "Cash" | "Card" | "Online";
+    status: "Paid" | "Pending" | "Refunded";
+    transactionId?: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const PaymentSchema = new Schema<IPayment>(
+    {
+        order: {
+            type: Schema.Types.ObjectId,
+            ref: "Order",
+            required: true,
+        },
+        reservation: {
+            type: Schema.Types.ObjectId,
+            ref: "Reservation",
+            default:null ,
+            required: false,
+        },
+        amount: {
+            type: Number,
+            required: true,
+        },
+        paymentMethod: {
+            type: String,
+            enum: ["Cash" , "Card" , "Online"],
+            default: "Cash",
+            required: true,
+        },
+        status: {
+            type: String,
+            enum: ["Paid" , "Pending" , "Refunded"],
+            default: "Pending",
+            required: true,
+        },
+        transactionId: {
+            type: String,
+            required: false,
+        },
+        updatedAt: {
+            type: Date,
+            default : Date.now,
+        },
+        createdAt:{
+            type:Date,
+        }
+    },
+    {
+        collection: "payment",
+        timestamps: true,
+    }
+);
+
+export const Payment = model<IPayment>("payment", PaymentSchema);
