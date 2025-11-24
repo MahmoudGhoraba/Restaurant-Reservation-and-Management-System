77954a615eec46ab1b03e1f59507643a810d7a20
AhmedHebesha <160162031+AhmedHebesha@users.noreply.github.com>
2025-11-24 15:06:49 +0200

    some changes

--- backend/src/application/routes/menu.ts
+++ backend/src/application/routes/menu.ts
@@ -4,7 +4,7 @@ import MenuItemController from "../controllers/menuitem.controller"
 const router: Router = express.Router()
 router.post('/',/*AUTH(ADMIN)*/ MenuItemController.createMenuItem)
 router.get('/:id', MenuItemController.getMenuItem)
-router.put('/:id',/*AUTH(ADMIN)*/ MenuItemController.updateMenuItem)
+router.patch('/:id',/*AUTH(ADMIN)*/ MenuItemController.updateMenuItem)
 router.delete('/:id',/*AUTH(ADMIN)*/ MenuItemController.deleteMenuItem)
 
 export default router
